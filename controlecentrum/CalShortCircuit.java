433794fca03fd99bd8ff9c3f174af383e2ab02b6
Patrick <alemanpatrick@gmail.com>
2013-01-07 21:00:25 +0100

    pseodo code voor paul

--- controlecentrum/CalShortCircuit.java
+++ controlecentrum/CalShortCircuit.java
@@ -1,11 +1,28 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-public class CalShortCircuit extends Calamities{
+public class CalShortCircuit extends Calamities {
+
+    /* AANMAKEN BENODIGDE VARIABLEN
+     * zoals tijd, timer etc.
+     * zorg dat de variable die je moet laten zien Static is
+     * deze kan je dan bereiken vannuit een andere class
+     */
+    
     public CalShortCircuit(){
         setImage("shortCircuit.png");
+        
+        // initialiseer je timer
     }
 
     public void act(){
-
+        /* start timer en controleer of de tijd verstreken is
+         * 
+         * IF tijdverstreken 
+         *    verwijder de calamiteit 
+         *    lose state, punten aftreken ...
+         * END IF
+         */
+        
     }    
+    
 }
